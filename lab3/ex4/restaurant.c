c3ef2ae03af5e018682023626d63ee16d91c074d
Natalie <nat130613@gmail.com>
2021-10-13 22:19:16 +0800

    lab3: fixed ex4 ex5

--- lab3/ex4/restaurant.c
+++ lab3/ex4/restaurant.c
@@ -8,16 +8,19 @@
 // You can declare global variables here
 #define TABLE_FREE 1
 #define TABLE_TAKEN 0
+#define MAX_TABLES 5000
 
 typedef struct
 {
-    sem_t block;       // to block people from taking tables
-    int num_tables;    // no of tables
-    int count_taken;   // no of tables taken
-    int count_waiting; // no of groups queuing
-    bool must_wait;    // true if all tables taken
-    int *table_ids;    // store the table ids
-    int *table_states;
+    sem_t block[MAX_TABLES]; // to block people from taking tables
+    int num_tables;          // no of tables
+    int count_taken;         // no of tables taken
+    int count_waiting;       // no of groups queuing
+    bool must_wait;          // true if all tables taken
+    int *table_ids;          // store the table ids
+    int *table_states;       // either taken or free
+    int waiting_head;        // for the waiting queue
+    int waiting_tail;        // for the waiting queue
 } queue_t;
 
 sem_t mutex;
@@ -28,7 +31,7 @@ void restaurant_init(int num_tables[5])
     // Write initialization code here (called once at the start of the program).
     // It is guaranteed that num_tables is an array of length 5.
     // TODO
-    int i, table_id = 0;
+    int i, j, table_id = 0;
     sem_init(&mutex, 0, 1);
 
     for (i = 0; i < 5; i++)
@@ -36,15 +39,19 @@ void restaurant_init(int num_tables[5])
         int n = num_tables[i];
         queue_t *queue = &queues[i];
 
-        sem_init(&queue->block, 0, 0);
+        for (j = 0; j < MAX_TABLES; j++)
+            sem_init(&queue->block[j], 0, 0);
+
         queue->num_tables = n;
         queue->count_taken = 0;
         queue->count_waiting = 0;
         queue->must_wait = false;
         queue->table_ids = (int *)malloc(n * sizeof(int));
         queue->table_states = (int *)malloc(n * sizeof(int));
+        queue->waiting_head = 0;
+        queue->waiting_tail = 0;
 
-        for (int j = 0; j < n; j++)
+        for (j = 0; j < n; j++)
         {
             queue->table_ids[j] = table_id;
             queue->table_states[j] = TABLE_FREE;
@@ -61,7 +68,8 @@ void restaurant_destroy(void)
     for (int i = 0; i < 5; i++)
     {
         queue_t *queue = &queues[i];
-        sem_destroy(&queue->block);
+        for (int j = 0; j < MAX_TABLES; j++)
+            sem_destroy(&queue->block[j]);
         free(queue->table_ids);
         free(queue->table_states);
     }
@@ -85,7 +93,9 @@ int request_for_table(group_state *state, int num_people)
         // block until table is available
         queue->count_waiting++;
         sem_post(&mutex);
-        sem_wait(&queue->block);
+        int k = queue->waiting_tail;
+        queue->waiting_tail++;
+        sem_wait(&queue->block[k]);
         queue->count_waiting--;
     }
 
@@ -105,7 +115,11 @@ int request_for_table(group_state *state, int num_people)
     queue->must_wait = (queue->count_taken == queue->num_tables);
 
     if (queue->count_waiting && (!queue->must_wait))
-        sem_post(&queue->block);
+    {
+        int k = queue->waiting_head;
+        queue->waiting_head++;
+        sem_post(&queue->block[k]);
+    }
     else
         sem_post(&mutex);
 
@@ -141,7 +155,11 @@ void leave_table(group_state *state)
     }
 
     if (queue->count_waiting && (!queue->must_wait))
-        sem_post(&queue->block);
+    {
+        int k = queue->waiting_head;
+        queue->waiting_head++;
+        sem_post(&queue->block[k]);
+    }
     else
         sem_post(&mutex);
 }
--- lab3/ex5/restaurant.c
+++ lab3/ex5/restaurant.c
@@ -8,16 +8,19 @@
 // You can declare global variables here
 #define TABLE_FREE 1
 #define TABLE_TAKEN 0
+#define MAX_TABLES 5000
 
 typedef struct
 {
-    sem_t block;       // to block people from taking tables
-    int num_tables;    // no of tables
-    int count_taken;   // no of tables taken
-    int count_waiting; // no of groups queuing
-    bool must_wait;    // true if all tables taken
-    int *table_ids;    // store the table ids
-    int *table_states;
+    sem_t block[MAX_TABLES]; // to block people from taking tables
+    int num_tables;          // no of tables
+    int count_taken;         // no of tables taken
+    int count_waiting;       // no of groups queuing
+    bool must_wait;          // true if all tables taken
+    int *table_ids;          // store the table ids
+    int *table_states;       // either taken or free
+    int waiting_head;        // for the waiting queue
+    int waiting_tail;        // for the waiting queue
 } queue_t;
 
 sem_t mutex;
@@ -30,7 +33,7 @@ void restaurant_init(int num_tables[5])
     // Write initialization code here (called once at the start of the program).
     // It is guaranteed that num_tables is an array of length 5.
     // TODO
-    int i, table_id = 0;
+    int i, j, table_id = 0;
     sem_init(&mutex, 0, 1);
 
     for (i = 0; i < 5; i++)
@@ -38,13 +41,17 @@ void restaurant_init(int num_tables[5])
         int n = num_tables[i];
         queue_t *queue = &queues[i];
 
-        sem_init(&queue->block, 0, 0);
+        for (j = 0; j < MAX_TABLES; j++)
+            sem_init(&queue->block[j], 0, 0);
+
         queue->num_tables = n;
         queue->count_taken = 0;
         queue->count_waiting = 0;
         queue->must_wait = false;
         queue->table_ids = (int *)malloc(n * sizeof(int));
         queue->table_states = (int *)malloc(n * sizeof(int));
+        queue->waiting_head = 0;
+        queue->waiting_tail = 0;
 
         for (int j = 0; j < n; j++)
         {
@@ -63,7 +70,8 @@ void restaurant_destroy(void)
     for (int i = 0; i < 5; i++)
     {
         queue_t *queue = &queues[i];
-        sem_destroy(&queue->block);
+        for (int j = 0; j < MAX_TABLES; j++)
+            sem_destroy(&queue->block[j]);
         free(queue->table_ids);
         free(queue->table_states);
     }
@@ -98,7 +106,9 @@ int request_for_table(group_state *state, int num_people)
         // block until table is available
         queue->count_waiting++;
         sem_post(&mutex);
-        sem_wait(&queue->block);
+        int k = queue->waiting_tail;
+        queue->waiting_tail++;
+        sem_wait(&queue->block[k]);
         queue->count_waiting--;
         if (swap_queue)
         {
@@ -127,7 +137,11 @@ enter:
     queue->must_wait = (queue->count_taken == queue->num_tables);
 
     if (queue->count_waiting && (!queue->must_wait))
-        sem_post(&queue->block);
+    {
+        int k = queue->waiting_head;
+        queue->waiting_head++;
+        sem_post(&queue->block[k]);
+    }
     else
         sem_post(&mutex);
 
@@ -162,17 +176,22 @@ void leave_table(group_state *state)
     }
     if (queue->count_waiting && (!queue->must_wait))
     {
-        sem_post(&queue->block);
+        int k = queue->waiting_head;
+        queue->waiting_head++;
+        sem_post(&queue->block[k]);
     }
     else if (queue->count_waiting == 0)
     {
         for (i = q_i - 1; i >= 0; i--)
         {
-            if (queues[i].count_waiting && queues[i].must_wait)
+            queue_t *q = &queues[i];
+            if (q->count_waiting && q->must_wait)
             {
                 swap_queue = true;
                 swap_q_i = q_i;
-                sem_post(&queues[i].block);
+                int k = q->waiting_head;
+                q->waiting_head++;
+                sem_post(&q->block[k]);
                 break;
             }
         }
